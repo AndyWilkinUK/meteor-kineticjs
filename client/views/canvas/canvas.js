2fb98abd42bc07493c38fc3920d92c8664b0e76f
Andrew Wilkin <andrew.wilkin@cyberion-consulting.co.uk>
2014-05-21 23:10:05 +0100

    successfully rendered to canvas and moved it around

--- client/views/canvas/canvas.js
+++ client/views/canvas/canvas.js
@@ -48,49 +48,47 @@ Template.Canvas.rendered = function() {
 
   Pictures.find().observe({
     added: function(doc) {
-      console.log('Added Called');
-      console.log(doc);
-      imageObj = new Image();
-      //console.log(doc.url);
-      //console.log(imageObj.src + ' hello');
-      imageObj.src = doc.url;
-      $('#container').append(imageObj);
-      imageObj.onload = function() {
-      console.log('here mum Im loading');
-      darth = new Kinetic.Image({
-        x: doc.x,
-        y: doc.y,
-        image: imageObj,
-        width: doc.width,
-        height: doc.height,
-        draggable: true
-      })
-        .on('dragmove', function() {
-          var newXY = {
-            x: darth.getAttr('x'),
-            y: darth.getAttr('y')
-          };
-          Puzzle.update(doc._id, {
-            $set: newXY
-          });
+      kineticImage = {};
+      kineticImageObj = new Image();
+      kineticImageObj.src = doc.url;
+      $('body').append(kineticImageObj);
+      kineticImageObj.onload = function() {
+        console.log('here mum Im loading');
+        kineticImage[doc._id] = new Kinetic.Image({
+          x: doc.x,
+          y: doc.y,
+          image: kineticImageObj,
+          width: doc.width,
+          height: doc.height,
+          draggable: true
         })
-        .on('dragend', function() {
-          var newXY = {
-            x: darth.getAttr('x'),
-            y: darth.getAttr('y')
-          };
-          Puzzle.update(doc._id, {
-            $set: newXY
+          .on('dragmove', function() {
+            var newXY = {
+              x: kineticImage[doc._id].getAttr('x'),
+              y: kineticImage[doc._id].getAttr('y')
+            };
+            Pictures.update(doc._id, {
+              $set: newXY
+            });
+          })
+          .on('dragend', function() {
+            var newXY = {
+              x: kineticImage[doc._id].getAttr('x'),
+              y: kineticImage[doc._id].getAttr('y')
+            };
+            Pictures.update(doc._id, {
+              $set: newXY
+            });
           });
-        });
 
-
-      layer.draw();
+        layer.add(kineticImage[doc._id]);
+        layer.draw();
       };
     },
     changed: function(doc) {
-      console.log('changed');
-      //square.setContent(clickCounter.number);
+      kineticImage[doc._id].setAttr('x', doc.x);
+      kineticImage[doc._id].setAttr('y', doc.y);
+      layer.draw();
     }
   });
 
